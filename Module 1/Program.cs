f1df7d4910277b15a5b3bb21ade591a852fc40b1
Khang Kyle Huynh <khanghuynh@Khangs-MacBook-Pro.local>
2025-02-14 14:00:39 -0600

    making repository

---
+++ Module 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.WriteLine("Khang's first assignement left to his own devices");
+Console.WriteLine("I'm afraid");
+
+class Circle
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Radius");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * radius * radius;
+        Console.WriteLine("The area of the circle is: " + area);
+    }
+
+    public class TriangleArea
+    { 
+        double b
+    }
+}
+
+
